72683fc7e4d126569b10430e455e7f75df54b9cf
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2022-05-20 12:16:24 +0300

    cards displayed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./components/header/Header";
 import { useEffect, useState } from "react";
 import axios from "axios";
-const API_URL = "http://www.omdbapi.com/?s=star wars&&apikey=d7b52bbd";
+import MoviesList from "./components/MoviesList";
+const API_URL = "http://www.omdbapi.com/?s=happy&&apikey=d7b52bbd";
 function App() {
   const [data, setData] = useState([]);
 
@@ -18,13 +19,7 @@ function App() {
     <div className="App">
       <Header />
       {console.log(data)}
-      {data &&
-        data.map((item, idx) => (
-          <div key={idx}>
-            <h6>{item.Title}</h6>
-            <img src={item.Poster} alt={item.Title} />
-          </div>
-        ))}
+      <MoviesList data={data} />
     </div>
   );
 }
---
+++ src/components/MoviesList.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+//again some style sshould be imported from another files...
+const Movie = styled.div`
+  overflow-x: auto;
+  flex-wrap: nowrap;
+  box-sizing: border-box;
+`;
+
+const Row = styled.div`
+  float: left;
+  width: 15%;
+  padding: 0 10px;
+  @media screen and (max-width: 1175px) {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+`;
+
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+`;
+
+const Img = styled.img`
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 300px;
+`;
+const MoviesList = ({ data }) => {
+  return (
+    <>
+      <Movie>
+        {data &&
+          data.map((item, idx) => (
+            <Row key={idx}>
+              <Card>
+                <h6>{item.Title}</h6>
+                <Img src={item.Poster} alt={item.Title} />
+              </Card>
+            </Row>
+          ))}
+      </Movie>
+    </>
+  );
+};
+
+export default MoviesList;
