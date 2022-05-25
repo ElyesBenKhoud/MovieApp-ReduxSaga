de995aa4cc617d66225f8343e898c52788cdddb3
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2022-05-25 14:31:34 +0300

    another API call

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 const API_URL = "http://www.omdbapi.com/?s=action&&apikey=d7b52bbd";
+const API_URL2 = "http://www.omdbapi.com/?s=love&&apikey=d7b52bbd";
+
 export const fetchMovies = async () => await axios.get(API_URL);
+
+export const fetchOtherMovies = async () => await axios.get(API_URL2);
 
 // const API_URL = "https://www.boredapi.com/api/activity";
 
