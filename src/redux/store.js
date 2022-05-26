9044f18327122c625b6dc8058a1dee1f69d74a08
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2022-05-26 17:12:01 +0300

    adding redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,16 @@ import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 // import { OnLoadMovie } from "./SagaMovies/sagas";
 import rootSaga from "./rootSaga";
 const sagaMiddleware = createSagaMiddleware();
 // const middlewares = [logger, sagaMiddleware];
 const middlewares = [thunk, sagaMiddleware, logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 sagaMiddleware.run(rootSaga);
 export default store;
