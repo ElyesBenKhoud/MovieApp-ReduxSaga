b09d70789546a9d107bdcba90c6c4d2bb4795a40
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2022-06-02 14:12:53 +0300

    clean header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,21 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Search from "./Search";
 import { DiJsBadge } from "react-icons/di";
-//style should be imported from another file
-// const HeaderWrapper = styled.header`
-//   display: flex;
-//   justify-content: space-between;
-//   flex-wrap: wrap;
-//   height: 90px;
-//   width: 100%;
-//   background: teal;
-//   @media screen and (max-width: 1183px) {
-//     height: 100%;
-//   }
-//   @media screen and (max-width: 865px) {
-//     height: 100%;
-//   }
-// `;
+
 
 const HeaderWrapper = styled.header`
   display: flex;
